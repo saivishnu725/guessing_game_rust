3b56b6c7cc1f8e7becbf5affe9b231b3d27e0cce
Sai Vishnu <saivishnu725@gmail.com>
2021-08-16 19:28:49 +0530

    Used ordering enums thingy

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 fn main() {
+    let random_number = rand::thread_rng().gen_range(1..=100);
+
     println!("Guess the number! ");
     println!("Input your guess. ");
     let mut guess = String::new();
@@ -7,4 +11,12 @@ fn main() {
         .read_line(&mut guess)
         .expect("Please give a string");
     println!("it is : {}", guess);
+    println!("Secret number is {} ", random_number);
+    let guess: i32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&random_number) {
+        Ordering::Equal => println!("You win!!"),
+        Ordering::Less => println!("Too small!!"),
+        Ordering::Greater => println!("Too big!!"),
+    }
 }
