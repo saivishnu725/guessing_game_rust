276d565df9b6949aed0132e4719d1ed9b8fc503d
Sai Vishnu <saivishnu725@gmail.com>
2021-08-16 17:00:53 +0530

    Input done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number! ");
+    println!("Input your guess. ");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Please give a string");
+    println!("it is : {}", guess);
 }
