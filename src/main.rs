26d63e6f6a517f35b8afb50e872d91b469d02498
Sai Vishnu <saivishnu725@gmail.com>
2021-08-16 20:43:28 +0530

    It works in a loop until correct numb is guessed

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,24 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 fn main() {
-    let random_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("Guess the number! ");
-    println!("Input your guess. ");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Please give a string");
-    println!("it is : {}", guess);
-    println!("Secret number is {} ", random_number);
-    let guess: i32 = guess.trim().parse().expect("Please type a number!");
-
-    match guess.cmp(&random_number) {
-        Ordering::Equal => println!("You win!!"),
-        Ordering::Less => println!("Too small!!"),
-        Ordering::Greater => println!("Too big!!"),
+    let random_number = rand::thread_rng().gen_range(1..=10);
+    loop {
+        println!("Guess the number! ");
+        println!("Input your guess. ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Please give a string");
+        println!("Secret number is {} ", random_number);
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        match guess.cmp(&random_number) {
+            Ordering::Equal => {
+                println!("You win!!");
+                break;
+            }
+            Ordering::Less => println!("Too small!!"),
+            Ordering::Greater => println!("Too big!!"),
+        }
+        println!("\n\n\n\n");
     }
 }
